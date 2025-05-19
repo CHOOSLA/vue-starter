4ff6b2d7d5dbc797d3434f20f184d5c4c273db9e
Choo ChangWoo <cnckddn0146@gmail.com>
2025-05-19 11:26:45 +0900

    Update WelcomePage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-  <h2>Vue + Vite + ESLint + Prettier </h2>
+  <h2>Vue + Vite + Tailwind + ESLint + Prettier </h2>
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
